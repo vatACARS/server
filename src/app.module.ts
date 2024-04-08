bae336b82df743d9de6c5d58e6bb92f4c82a563b
Joshua Hughes <joshua@plutonus.dev>
2024-04-08 20:40:07 +1000

    feat: add mongoose and connect to DB
    This commit includes the following changes:
    - Adds mongoose package to the project to facilitate MongoDB interactions.
    - Modifies the app.module to import MongooseModule and connect to the database using a URL from the config file.
    - Adds config.ts to the .gitignore file to prevent sensitive data (like the database URL) from being committed.
    - Creates a config.ts.example file for reference of the required structure of the config.ts file.
    - Updates the package.json and yarn.lock files to reflect the newly added dependencies.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { DB_URL } from "./config";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
