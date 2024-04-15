18f43a2f5b977d200191850b392ded57bc1aa898
Joshua Hughes <joshua@plutonus.dev>
2024-04-16 07:27:22 +1000

    fix: correct version number and empty changes array in getLatestVersion function

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,14 +4,8 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getLatestVersion(): object {
     return {
-      "version": "1.0.1",
-      "changes": [
-        "+ change 1",
-        "- change 2",
-        "+ testing  1 2 3",
-        "+ cool cool cool",
-        "+ and we're done here, thank you!"
-      ],
+      "version": "1.0.0",
+      "changes": [],
       "releaseDateTime": "2024-04-08T09:08:26.407Z"
     };
   }
