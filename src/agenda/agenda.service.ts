0620ecc330d217c462a2b9386ffd8030a8d83cdd
Joshua Hughes <joshua@plutonus.dev>
2024-06-12 03:58:55 +1000

    feat: add logout functionality for inactive ATSU in AgendaService

--- src/agenda/agenda.service.ts
+++ src/agenda/agenda.service.ts
@@ -1,8 +1,27 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import Agenda from "agenda";
+
+import { ATSUInformation } from '@prisma/client';
+import { ATSUService } from 'src/prisma/atsu.service';
 
 @Injectable()
 export class AgendaService implements OnModuleInit {
-  async onModuleInit() {
-    return;
-  }
+    agenda: Agenda;
+    
+    constructor(
+        private readonly atsuService: ATSUService
+    ) {
+        this.agenda = new Agenda({ db: { address: process.env.database_url, collection: "tasks" } });
+    }
+
+    async onModuleInit() {
+        this.agenda.define("logout inactive ATSU", async job => {
+            const { station_code } = job.attrs.data;
+
+            const ATSUStation: ATSUInformation = await this.atsuService.ATSUInformation({ station_code });
+            if(ATSUStation) await this.atsuService.deleteATSUInformation({ station_code });
+
+            job.delete();
+        });
+    }
 }
