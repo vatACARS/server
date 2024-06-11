f1bb31bd9791288dff82b689d6f84489f92cb59e
Joshua Hughes <joshua@plutonus.dev>
2024-06-12 05:53:24 +1000

    fix: update default status and version in app.service.ts
    Changed default status from "waiting" to "disabled" and updated selectedVersion from 0 to -1 in app.service.ts. Also, modified default label and version in versions array.

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,12 +20,12 @@ export class AppService {
       name: "Controller",
       localVersion: "0.0.1",
       description: "The vatACARS plugin for vatSys is a faithfully recreated CPDLC interface designed to seamlessly assist with a controller's workflow. Providing a direct integration with Hoppies ACARS, controllers are immediately able to connect with pilots that are using either our pilot client or one of many others publicly available.",
-      status: "waiting",
-      selectedVersion: 0,
+      status: "disabled",
+      selectedVersion: -1,
       versions: [{
         id: 0,
-        label: "Stable",
-        version: "0.0.1"
+        label: "Not Available",
+        version: "X.X.X"
       }/*, {
           id: 1,
           label: "Bleeding Edge",
