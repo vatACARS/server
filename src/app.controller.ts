e2cf3ecf0489c8e4677ec8d5ed8e7bef5c4bb16f
Joshua Hughes <joshua@plutonus.dev>
2024-06-12 01:07:22 +1000

    feat: add ATSU service and message service to app controller and module
    This commit includes the integration of ATSU and ATSUMessage services to the app controller and app module. Now, the app is capable of handling and sending ATSU messages. Adjustments were made to the Post and Get methods in the app controller to accommodate these changes.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,17 @@ import { Response } from 'express';
 import { Controller, Get, Post, Body, Param, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
-import { MessageDTO } from './messages/message.dto';
+import { ATSUService } from "./prisma/atsu.service";
+import { ATSUMessageService } from "./prisma/atsuMessage.service";
+import { ATSUInformation as ATSUInformationModel, ATSUMessage as ATSUMessageModel } from "@prisma/client";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly atsuService: ATSUService,
+    private readonly atsuMessageService: ATSUMessageService
+  ) {}
 
   @Get('/foo')
   getFoo(): string {
@@ -28,13 +34,24 @@ export class AppController {
     return this.appService.getRepository();
   }
 
-  @Post('/messages/send')
-  postMessage(@Body() message: MessageDTO): object {
-    return {};
+  @Post('/atsu/logon')
+  async postLogon(@Body() body: any): Promise<ATSUInformationModel | any> {
+    const { token, station } = body;
+    const ACARSUserData = await fetch(`https://vatacars.com/api/client/me?token=${token}`).then(resp => resp.json());
+    if(!ACARSUserData.success) return { success: false, message: "Not authorised." };
+
+    const CurATSUStation = await this.atsuService.ATSUInformation({ station_code: station });
+    if(CurATSUStation) return { success: false, message: `${station} is already opened by CID ${CurATSUStation.acars_user_id}` };
+
+    return this.atsuService.createATSUInformation({
+      station_code: station,
+      opened: new Date(),
+      acars_user_id: ACARSUserData.vatACARSUserData.data.cid
+    })
   }
 
-  @Get('/messages/poll/:station')
-  getMessages(@Param() params: MessageDTO): object {
-    return { station: params.station };
+  @Get('/atsu/poll/:station')
+  async getMessages(@Param("station") station: string): Promise<ATSUMessageModel[]> {
+    return this.atsuMessageService.ATSUMessages({ where: { station }});
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { ATSUService } from './prisma/atsu.service';
+import { ATSUMessageService } from './prisma/atsuMessage.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { DB_URL } from './config';
@@ -8,6 +11,11 @@ import { DB_URL } from './config';
 @Module({
   imports: [MongooseModule.forRoot(DB_URL)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    PrismaService,
+    ATSUService,
+    ATSUMessageService
+  ],
 })
 export class AppModule {}
