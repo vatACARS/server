ef2214c6a6ff4c71b7b096c09b49c5d7b5160eed
Joshua Hughes <joshua@plutonus.dev>
2024-06-12 04:21:51 +1000

    refactor: implement OnModuleInit in AgendaService
    Updated the AgendaService to implement OnModuleInit. This allows the agenda to start upon module initialization. Also, added a new async function 'onModuleInit'.

--- src/agenda/agenda.service.ts
+++ src/agenda/agenda.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 const Agenda = require("agenda");
 
 import { ATSUInformation } from '@prisma/client';
 import { ATSUService } from 'src/prisma/atsu.service';
 
 @Injectable()
-export class AgendaService {
+export class AgendaService implements OnModuleInit {
     agenda: any;
     
     constructor(
         private readonly atsuService: ATSUService
     ) {
         this.agenda = new Agenda({ db: { address: process.env.database_url, collection: "tasks" } });
+        this.agenda.start();
+    }
 
+    async onModuleInit() {
         this.agenda.define("logout inactive ATSU", async job => {
             const { station_code } = job.attrs.data;
 
