9f4b43f5fa4949c076f8b4d2b01ecaf2ca04a339
Joshua Hughes <joshua@plutonus.dev>
2024-06-16 05:31:01 +1000

    Update info

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,45 +4,42 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getLatestHubVersion(): string {
     // TDB: Dynamically grab this from CDN
-    return "0.1.2";
+    return "1.0.0";
   }
 
   getLatestVersion(): object {
     return {
-      version: '1.0.0',
-      changes: ["Initial release of the vatACARS plugin for vatSys."],
-      releaseDateTime: '2024-06-14T03:54:14.433Z',
+      version: '1.0.1',
+      changes: ["Enjoy Cross the Ditch!"],
+      releaseDateTime: '2024-06-15T19:30:08.943Z',
     };
   }
 
   getAvailableClients(): object {
     return [{
-      name: "Controller",
-      localVersion: "",
-      description: "The vatACARS plugin for vatSys is a faithfully recreated CPDLC interface designed to seamlessly assist with a controller's workflow. Providing a direct integration with Hoppies ACARS, controllers are immediately able to connect with pilots that are using either our pilot client or one of many others publicly available.",
-      status: "disabled",
-      selectedVersion: -1,
-      versions: [{
-        id: 0,
-        label: "Early Access",
-        version: "1.0.1"
-      }/*, {
-          id: 1,
-          label: "Bleeding Edge",
-          version: "0.0.1-be"
-      }*/]
-    }, {
-      name: "Pilot",
-      localVersion: "",
-      description: "To be announced!",
-      status: "disabled",
-      selectedVersion: -1,
-      versions: [{
-        id: 0,
-        label: "Not Available",
-        version: "X.X.X"
-      }]
-    }]
+      name: "Controller Plugin",
+      bgImageUrl: "https://images.unsplash.com/photo-1646768843273-4dbb7a174a4d?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      latestVersion: "1.0.1",
+      description: "The vatACARS plugin for vatSys is a purpose-built CPDLC interface that helps streamline a controller's workflow. It connects directly with Hoppies ACARS, allowing controllers to easily link up with pilots using our pilot client or various other available options.",
+      latestChangelog: [{
+          logType: 3,
+          label: "There's nothing here yet."
+      }],
+      availableDownloads: [{
+          version: "1.0.1",
+          label: "Early Access"
+      }],
+      selectedDownload: 0,
+      downloadUrl: "https://dist.vatacars.com/plugin/vatACARS.dll"
+  }, {
+      name: "Pilot Client",
+      bgImageUrl: "https://plus.unsplash.com/premium_photo-1661963446458-274e8810cf59?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      latestVersion: ""
+  }, {
+      name: "Dispatcher Center",
+      bgImageUrl: "https://images.unsplash.com/photo-1580795479225-c50ab8c3348d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      latestVersion: ""
+  }]
   }
 
   getRepository(): object {
