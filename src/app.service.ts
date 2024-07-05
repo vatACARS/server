2ff1598409b44c8e545e8d7bdc9f5aba5ca189fa
Joshua Hughes <joshua@plutonus.dev>
2024-07-05 22:47:36 +1000

    fix: Update AppService to handle version 1.0.2 changes
    This commit updates the AppService to reflect the changes made in version 1.0.2. It includes fixes for PDC crashes and quickResponse crashes, as well as miscellaneous bug fixes and quality of life improvements. The latest version number and release date time have also been updated.

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,9 +9,9 @@ export class AppService {
 
   getLatestVersion(): object {
     return {
-      version: '1.0.1',
-      changes: ["Enjoy Cross the Ditch!"],
-      releaseDateTime: '2024-06-15T19:30:08.943Z',
+      version: '1.0.2',
+      changes: ["Fixed PDC crashes", "Fixed quickResponse crashes", "Misc bug fixes", "QOL improvements"],
+      releaseDateTime: '2024-07-05T12:47:20.307Z',
     };
   }
 
@@ -19,14 +19,23 @@ export class AppService {
     return [{
       name: "Controller Plugin",
       bgImageUrl: "https://images.unsplash.com/photo-1646768843273-4dbb7a174a4d?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      latestVersion: "1.0.1",
+      latestVersion: "1.0.2",
       description: "The vatACARS plugin for vatSys is a purpose-built CPDLC interface that helps streamline a controller's workflow. It connects directly with Hoppies ACARS, allowing controllers to easily link up with pilots using our pilot client or various other available options.",
       latestChangelog: [{
+          logType: 1,
+          label: "Fixed crashes caused by aircraft requesting PDCs with no FDR"
+      }, {
+          logType: 1,
+          label: "Fixed crashes caused by quick response placeholder messages loading incorrectly"
+      }, {
           logType: 3,
-          label: "There's nothing here yet."
+          label: "Other miscellaneous bug fixes"
+      }, {
+          logType: 3,
+          label: "QOL improvements"
       }],
       availableDownloads: [{
-          version: "1.0.1",
+          version: "1.0.2",
           label: "Early Access"
       }],
       selectedDownload: 0,
