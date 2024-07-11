edfe831a07c51294142ea775c51428614e530e0e
Joshua Hughes <joshua@plutonus.dev>
2024-07-11 22:22:55 +1000

    feat: Added Swagger documentation to the server API
    This commit includes the addition of Swagger documentation to the vatACARS server API, providing RESTful API access and improved user interaction with the vatACARS plugin for vatSys, pilot client, and dispatcher center. The documentation version has been updated to 0.0.2.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const config = new DocumentBuilder()
+    .setTitle('vatACARS Server API')
+    .setDescription(
+      'The vatACARS Server API is a RESTful API that provides access to the vatACARS server. This API is used by the vatACARS plugin for vatSys, the vatACARS pilot client, and the vatACARS dispatcher center.',
+    )
+    .setVersion('0.0.2')
+    .addTag('vatACARS')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(8002);
 }
+
 bootstrap();
