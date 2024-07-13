ea670dd870ee4aa261e13a6544e9f1e2682841e4
Joshua Hughes <joshua@plutonus.dev>
2024-07-13 18:59:16 +1000

    refactor: Restructure app.module.ts for better organization
    Refactored the app.module.ts file for better organization. Removed unnecessary controller and service imports and added an import for the new ATSUModule. The controllers and providers have also been cleaned up for a more streamlined structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ATSUModule } from './ATSU/atsu.module';
+
 import { AppController } from './app.controller';
-import { ATSUController } from './ATSU/atsu.controller';
 
 import { AppService } from './app.service';
 import { AgendaService } from './lib/agenda/agenda.service';
 import { PrismaService } from './lib/prisma/prisma.service';
-import { ATSUService } from './lib/prisma/atsu.service';
 import { ATSUMessageService } from './lib/prisma/atsuMessage.service';
 
 import { DB_URL } from './config';
 
 @Module({
-  imports: [MongooseModule.forRoot(DB_URL)],
-  controllers: [
-    AppController,
-    ATSUController
+  imports: [
+    MongooseModule.forRoot(DB_URL),
+    ATSUModule
   ],
+  controllers: [AppController],
   providers: [
     AppService,
     AgendaService,
     PrismaService,
-    ATSUService,
     ATSUMessageService
   ],
 })
