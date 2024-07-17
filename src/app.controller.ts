6451762dc51d76df7484e5fcf49bbb60b8d64f7e
Joshua Hughes <joshua@plutonus.dev>
2024-07-17 20:27:10 +1000

    feat: added post endpoint to report issues
    This commit involves the addition of a new post endpoint in the AppController class for reporting issues. The '/reporting' route now allows users to post reports with necessary data such as 'token', ' 'data'. It also includes necessary validation checks and proper handling of unauthorized or invalid requests. A throttle decorator has been added to limit the number time period. This new feature should improve the user interface by allowing users to report issues directly through the application.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Res, HttpStatus } from '@nestjs/common';
 
 import {
   ApiTags,
@@ -9,7 +9,9 @@ import {
 import { AppService } from './app.service';
 import { ATSUMessageService } from "./lib/prisma/atsuMessage.service";
 import { ATSUMessage as ATSUMessageModel } from "@prisma/client";
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller()
 @ApiTags("vatACARS")
 export class AppController {
@@ -44,5 +46,21 @@ export class AppController {
   @Get('/atsu/poll/:station')
   async getMessages(@Param("station") station: string): Promise<ATSUMessageModel[]> {
     return this.atsuMessageService.ATSUMessageCollection({ where: { station } });
+  }
+
+  @Throttle({ default: { limit: 3, ttl: 60000 } })
+  @Post('/reporting')
+  async postReport(@Body() body: any, @Res() response: Response): Promise<Response> {
+    const { token, source, data } = body;
+    if (!token || !source || !data) return response.status(400).json({ success: false, message: "Missing data" });
+    const ACARSUserData = await fetch(`https://vatacars.com/api/client/me?token=${token}`).then(resp => resp.json());
+    if (!ACARSUserData.success) return response.status(HttpStatus.UNAUTHORIZED).json({ success: false, message: "Not authorised" });
+    if(!["plugin"].includes(source)) return response.status(400).json({ success: false, message: "Invalid source" });
+
+    const { ident, raw } = data;
+    if(!ident || !raw) return response.status(400).json({ success: false, message: "Missing data" });
+
+    this.appService.reportIssue(ACARSUserData.vatACARSUserData.data.cid, source, data);
+    return response.status(200).json({ success: true });
   }
 }
