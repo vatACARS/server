01e61ddcf52bc1cb254ecf6ce5043cc7e9604ac0
Joshua Hughes <joshua@plutonus.dev>
2024-08-11 11:14:09 +1000

    test: add placeholder test case in app controller spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,20 +1,3 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 describe('AppController', () => {
-  let appController: AppController;
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    // TODO: Write some tests
-  });
+  it('to be done', () => expect(true).toBe(true));
 });
